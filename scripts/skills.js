528160f8110018a1efd2e5eec69c63dadddbb103
Phelipe Matheus Andrade <111017521+PhelipeMatheus13@users.noreply.github.com>
2024-09-23 19:37:16 -0300

    first commit

---
+++ scripts/skills.js
@@ -0,0 +1,231 @@
+var btnHtml = document.querySelector('#btn-hmtl'); 
+var btnCss = document.querySelector('#btn-css'); 
+var btnJs = document.querySelector('#btn-js'); 
+var btnGithub = document.querySelector('#btn-github'); 
+var btnJava = document.querySelector('#btn_java');
+var btnCsharp = document.querySelector('#btn_csharp');
+var btnSql = document.querySelector('#btn_sql');
+var btnCmais = document.querySelector('#btn_c_mais_mais');
+
+var cardBase = document.querySelector('#card-base');
+var cardHtml = document.querySelector('#card-html');
+var cardCss = document.querySelector('#card-css');
+var cardJs = document.querySelector('#card-js');
+var cardGithub = document.querySelector('#card-GitHub');
+var cardJava = document.querySelector('#card-java');
+var cardCsharp = document.querySelector('#card-csharp');
+var cardSql = document.querySelector('#card-sql')
+var cardCmais = document.querySelector('#card_c_mais_mais')
+
+
+// criando função botao HTML
+btnHtml.addEventListener('click', function() {
+
+    if(cardHtml.style.display === 'none') {
+        cardHtml.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+
+    } else {
+        cardHtml.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+    }
+});
+
+// criando função botao css
+btnCss.addEventListener('click', function() {
+
+    if(cardCss.style.display === 'none') {
+        cardCss.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+
+    } else {
+        cardCss.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+    }
+});
+
+// criando função botao JS
+btnJs.addEventListener('click', function() {
+
+    if(cardJs.style.display === 'none') {
+        cardJs.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+
+    } else {
+        cardJs.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+    }
+});
+
+// criando função botao GIT HUb
+btnGithub.addEventListener('click', function() {
+
+    if(cardGithub.style.display === 'none') {
+        cardGithub.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+
+    } else {
+        cardGithub.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+    }
+});
+
+// criando função botao JAVA
+btnJava.addEventListener('click', function() {
+
+    if(cardJava.style.display === 'none') {
+        cardJava.style.display = 'block';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+    } else {
+        cardJava.style.display = 'block';
+        cardGithub.style.display = 'none';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+    }
+});
+
+// criando função botao C sharp
+btnCsharp.addEventListener('click', function() {
+
+    if(cardCsharp.style.display === 'none') {
+        cardCsharp.style.display = 'block';
+        cardJava.style.display = 'none';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+    } else {
+        cardCsharp.style.display = 'block';
+        cardJava.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardSql.style.display = 'none';
+        cardCmais.style.display = 'none';
+    }
+});
+
+// criando função botao SQL
+btnSql.addEventListener('click', function() {
+
+    if(cardSql.style.display === 'none') {
+        cardSql.style.display = 'block';
+        cardCsharp.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardCmais.style.display = 'none';
+    } else {
+        cardSql.style.display = 'block';
+        cardCsharp.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardCmais.style.display = 'none';
+    }
+});
+
+// criando função botao C++
+btnCmais.addEventListener('click', function() {
+
+    if(cardCmais.style.display === 'none') {
+        cardCmais.style.display = 'block';
+        cardSql.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+        cardGithub.style.display = 'none';
+    } else {
+        cardCmais.style.display = 'block';
+        cardSql.style.display = 'none';
+        cardCsharp.style.display = 'none';
+        cardJava.style.display = 'none';
+        cardGithub.style.display = 'none';
+        cardBase.style.display = 'none';
+        cardHtml.style.display = 'none';
+        cardCss.style.display = 'none';
+        cardJs.style.display = 'none';
+    }
+});
